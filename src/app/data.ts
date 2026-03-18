7d2f2e58e9a7fb4af0b9e3fa6ad447f344a497d6
kcjeon83 <kcjeon83@naver.com>
2026-03-18 17:29:04 +0900

    img2 추가

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,8 +1,9 @@
 import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
 
 export const GALLERY_ITEMS = [
   { id: 1, src: img1, title: '25th The Legacy', subtitle: '25년의 경의, 멈추지 않는 전설' },
-  { id: 2, src: 'https://images.unsplash.com/photo-1756451182341-6e62d5ec9697?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxlZGl0b3JpYWwlMjBmYXNoaW9uJTIwbW9ub2Nocm9tZXxlbnwxfHx8fDE3NzM4MTAxNzJ8MA&ixlib=rb-4.1.0&q=80&w=1080', title: 'Avant Garde', subtitle: 'Editorial Fashion' },
+  { id: 2, src: img2, title: '미르 비화록 2', subtitle: '26년미르가 나아갈 길' },
   { id: 3, src: 'https://images.unsplash.com/photo-1640841932394-7e135c0c6c3c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzY3VscHR1cmUlMjBtb2Rlcm58ZW58MXx8fHwxNzczODEwMTcyfDA&ixlib=rb-4.1.0&q=80&w=1080', title: 'Modern Form', subtitle: 'Sculpture & Texture' },
   { id: 4, src: 'https://images.unsplash.com/photo-1647791770329-374795b33f8a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhYnN0cmFjdCUyMGxpZ2h0JTIwc2hhZG93fGVufDF8fHx8MTc3Mzc2ODkzMHww&ixlib=rb-4.1.0&q=80&w=1080', title: 'Lumière', subtitle: 'Abstract Shadows' },
   { id: 5, src: 'https://images.unsplash.com/photo-1629740936456-4b990c27e503?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhZXN0aGV0aWMlMjBwb3J0cmFpdHxlbnwxfHx8fDE3NzM3NTY2MzN8MA&ixlib=rb-4.1.0&q=80&w=1080', title: 'Silhouette', subtitle: 'Aesthetic Portrait' },
@@ -10,4 +11,3 @@ export const GALLERY_ITEMS = [
   { id: 7, src: 'https://images.unsplash.com/photo-1569091791842-7cfb64e04797?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb250ZW1wb3JhcnklMjBhcnR8ZW58MXx8fHwxNzczNzU2NjI1fDA&ixlib=rb-4.1.0&q=80&w=1080', title: 'Abstract Tone', subtitle: 'Contemporary Art' },
   { id: 8, src: 'https://images.unsplash.com/photo-1604147706283-d7119b5b822c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx0ZXh0dXJlZCUyMGJhY2tncm91bmR8ZW58MXx8fHwxNzczODEwMTcyfDA&ixlib=rb-4.1.0&q=80&w=1080', title: 'Surface', subtitle: 'Tactile Studies' }
 ];
-//test
